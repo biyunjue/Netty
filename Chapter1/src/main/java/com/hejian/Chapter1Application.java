10290d9e0abd9252bf637b580dfcba91031ef5e0
biyunjue <biyunfengjue@gmail.com>
2017-12-27 12:58:34 +0800

    the first submit
    the first submit

---
+++ Chapter1/src/main/java/com/hejian/Chapter1Application.java
@@ -0,0 +1,15 @@
+package com.hejian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hj
+ */
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+}
